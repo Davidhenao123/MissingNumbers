a5b9a906def1ba4a4fde1986ddb45df54e5ce40a
Juan David Henao Suarez <33737082+Davidhenao123@users.noreply.github.com>
2020-03-07 16:39:05 -0500

    Se adiciona archivos
    Se adiciona archivos

---
+++ MissingNumbers.UnitTest/TestCase0.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MissingNumbers.Business;
+using System;
+
+namespace MissingNumbers.UnitTest
+{
+    [TestClass]
+    public class TestCase0
+    {
+        private IController business = new Controller();
+
+        /// <summary>
+        /// Caso de prueba numero uno.
+        /// </summary>
+        [TestMethod]
+        public void TestCase0Method()
+        {
+            String target = "204 205 206";
+            String input = "10";
+            input += "\n203 204 205 206 207 208 203 204 205 206";
+            input += "\n13";
+            input += "\n203 204 204 205 206 207 205 208 203 206 205 206 204";
+            String response = business.SearchMissingNumbersChain(input);
+            Assert.AreEqual(response, target);
+        }
+    }
+}
---
+++ MissingNumbers.UnitTest/TestCase1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MissingNumbers.Business;
+using System;
+
+namespace MissingNumbers.UnitTest
+{
+    [TestClass]
+    public class TestCase1
+    {
+        private IController business = new Controller();
+        /// <summary>
+        /// Caso de prueba numero dos.
+        /// </summary>
+        [TestMethod]
+        public void TestCase0Method()
+        {
+            String target = "3 7 8 10 12";
+            String input = "10";
+            input += "\n11 4 11 7 13 4 12 11 10 14";
+            input += "\n15";
+            input += "\n11 4 11 7 3 7 10 13 4 8 12 11 10 14 12";
+            String response = business.SearchMissingNumbersChain(input);
+            Assert.AreEqual(response, target);
+        }
+    }
+}
