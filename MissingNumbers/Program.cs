ef3808d6a30e8b3db34c104c933ce8ab18e9aefe
Juan David Henao Suarez <33737082+Davidhenao123@users.noreply.github.com>
2020-03-07 16:37:39 -0500

    Se adiciona archivos
    Se adiciona archivos

---
+++ MissingNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MissingNumbers
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            catch (Exception ex)
+            {
+                Business.Utilities.WriteLog(ex.Message);
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
